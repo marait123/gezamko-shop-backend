95f691ff925dc96c4b03286e7dc6b065dfba0c16
marait123 <mohammedibrahimgaballah@gmail.com>
2025-12-28 23:06:20 +0200

    add category and brand filters to ProductFilter

--- products/filters.py
+++ products/filters.py
@@ -6,6 +6,8 @@ from .models import Product
 class ProductFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+    category = django_filters.CharFilter(field_name="category", lookup_expr="iexact")
+    brand = django_filters.CharFilter(field_name="brand", lookup_expr="iexact")
 
     class Meta:
         model = Product
