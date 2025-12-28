41aa09fc0c7f2e6cda501d31296d79a7246736cd
Noureldin7 <noureldin2671@gmail.com>
2025-12-29 00:46:08 +0200

    Perform Basic Authentication before Session Authentication

--- gezamko/settings.py
+++ gezamko/settings.py
@@ -159,8 +159,8 @@ AUTH_USER_MODEL = "users.User"
 # Django REST Framework
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
